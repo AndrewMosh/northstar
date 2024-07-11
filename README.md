5aa2b9ff44f1491d787ad072749740344324428a
Andrew Mosh <malinaenglishclub@gmail.com>
2024-07-11 12:03:47 +0300

    header added

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
-## Commands
-
 npm i
 
 npm run initHTML
 
-npm run initTS или npm run initJS
+npm run initJS
 
-npm run start (режим разработки)
+npm run start
 
-npm run build
+Запускаем конвертацию в формат .woff(.woff2) `npm run ttfToWoff`
+Подключаем шрифты в mixin scss `"app/styles/fonts.scss"` командой `npm run fontsInStyle`
+
+```js
+    scss: {
+        "@Sections": "./../pages/sections",
+        "@Components": "./../pages/components",
+        "@Elements": "./../pages/elements",
+    },
+    js: {
+        "@Sections": path.resolve(__dirname, "../../" + paths.sections),
+        "@Components": path.resolve(__dirname, "../../" + paths.components),
+        "@Elements": path.resolve(__dirname, "../../" + paths.elements),
+    },
+```
--- app/scripts/menu.ts
+++ app/scripts/menu.ts
@@ -1,33 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let isMenuOpen = false;
-    const burger = document.querySelector('.header__burger');
-    const nav = document.querySelector('.header__nav');
-    const links = document.querySelectorAll<HTMLElement>('.nav__link');
+const burger = document.querySelector('.header__burger');
+let isMenuOpen = false;
+const menu = document.querySelector('.header__menu');
+const links = document.querySelectorAll<HTMLElement>('.header__link');
+const logo = document.querySelector<HTMLImageElement>('.header__img');
 
+burger.addEventListener('click', () => {
+    isMenuOpen = !isMenuOpen;
 
-    burger.addEventListener('click', () => {
-        isMenuOpen = !isMenuOpen;
-        if (isMenuOpen) {
-            document.body.style.overflow = 'hidden';
-            nav.classList.toggle('header__nav--active');
-        } else {
-            document.body.style.overflow = 'auto';
-            nav.classList.toggle('header__nav--active');
-        }
-
-    });
-
-    if (links && nav) {
-        links.forEach((link) => {
-            link.addEventListener('click', () => {
-                isMenuOpen = !isMenuOpen;
-                document.body.style.overflow = 'auto';
-                nav.classList.toggle('header__nav--active');
-            });
-        });
+    if (isMenuOpen) {
+        document.body.style.overflow = 'hidden';
+        menu.classList.toggle('active');
+        logo.src = '../images/svg/logo-white.svg';
+    } else {
+        document.body.style.overflow = 'auto';
+        menu.classList.toggle('active');
+        logo.src = '../images/svg/logo.svg';
     }
 
+
 });
+
+if (links && menu) {
+    links.forEach((link) => {
+        link.addEventListener('click', () => {
+            isMenuOpen = !isMenuOpen;
+            document.body.style.overflow = 'auto';
+            menu.classList.toggle('active');
+        });
+    });
+}
+
 
 
 
